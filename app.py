c7e781155ee412dbfb7c86a1c217957fb921efb5
arieabisha <133473088+arieabisha@users.noreply.github.com>
2024-03-24 09:45:40 +0000

    Update text

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello_world():
-  return "<p>Hello world</p>"
+  return "<p>Hello Abisha</p>"
 
 if __name__ == '__main__' :
   app.run(host='0.0.0.0',debug=True)
