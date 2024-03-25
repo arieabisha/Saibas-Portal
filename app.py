a9e788ea01b87833192ff2f37e48af0cfe9614a8
arieabisha <133473088+arieabisha@users.noreply.github.com>
2024-03-25 05:58:56 +0000

    careerlist added

--- app.py
+++ app.py
@@ -2,10 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Preschool Teacher',
+    'location': 'Jakarta, Indonesia',
+    'salary': 'IDR 6.000.000 - IDR 9.000.000'
+  },
+  {
+    'id': 1,
+    'title': 'PE Teacher',
+    'location': 'Jakarta, Indonesia',
+    'salary': 'IDR 8.000.000 - IDR 11.000.000'
+  },
+  {
+    'id': 1,
+    'title': 'Web developer',
+    'location': 'Jakarta, Indonesia',
+    'salary': 'IDR 11.000.000 - IDR 14.000.000'
+  }
+]
+
+
+
 @app.route("/")
 
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS , company_name='Saibas')
 
 if __name__ == '__main__' :
   app.run(host='0.0.0.0',debug=True)
